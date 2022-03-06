7aa42b895d2c3ecc38ada38e0cef9c649f915824
seunghabaik <slug0326@naver.com>
2022-03-07 01:30:46 +0900

    frontcontrollerservlet pattern v3 - servlet 종속성 제거, 뷰 경로 중복 제거

--- src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -17,13 +17,13 @@ import java.util.Map;
 @WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
 public class FrontControllerServletV2 extends HttpServlet {
 
-    private Map<String, ControllerV2> controllerV1Map = new HashMap<>();
+    private Map<String, ControllerV2> controllerV2Map = new HashMap<>();
 
     //생성자 setting
     public FrontControllerServletV2() {
-        controllerV1Map.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
-        controllerV1Map.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
-        controllerV1Map.put("/front-controller/v2/members", new MemberListControllerV2());
+        controllerV2Map.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerV2Map.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerV2Map.put("/front-controller/v2/members", new MemberListControllerV2());
     }
 
     @Override
@@ -31,7 +31,7 @@ public class FrontControllerServletV2 extends HttpServlet {
 
         String requestURI = request.getRequestURI();
 
-        ControllerV2 controller = controllerV1Map.get(requestURI);
+        ControllerV2 controller = controllerV2Map.get(requestURI);
 
         //예외 처리
         if (controller == null) {
