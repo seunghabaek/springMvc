5c62538514efa8396c4a1521176cd980b39758a4
seunghabaik <slug0326@naver.com>
2022-03-14 01:40:25 +0900

    frontcontrollerservlet pattern v5 - 유연하게 controller 변경

--- src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -1,14 +1,14 @@
-package hello.servlet.web.frontcontroller.v3;
+package hello.servlet.web.frontcontroller.v4;
 
 import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.v2.ControllerV2;
-import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
-import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
-import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,16 +19,16 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
-public class FrontControllerServletV3 extends HttpServlet {
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
 
-    private Map<String, ControllerV3> controllerV3Map = new HashMap<>();
+    private Map<String, ControllerV4> controllerV4Map = new HashMap<>();
 
     //생성자 setting
-    public FrontControllerServletV3() {
-        controllerV3Map.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
-        controllerV3Map.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
-        controllerV3Map.put("/front-controller/v3/members", new MemberListControllerV3());
+    public FrontControllerServletV4() {
+        controllerV4Map.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerV4Map.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerV4Map.put("/front-controller/v4/members", new MemberListControllerV4());
     }
 
     @Override
@@ -36,7 +36,7 @@ public class FrontControllerServletV3 extends HttpServlet {
 
         String requestURI = request.getRequestURI();
 
-        ControllerV3 controller = controllerV3Map.get(requestURI);
+        ControllerV4 controller = controllerV4Map.get(requestURI);
 
         //예외 처리
         if (controller == null) {
@@ -47,12 +47,12 @@ public class FrontControllerServletV3 extends HttpServlet {
         //paramMap
 
         Map<String, String> paramMap = createParamMap(request);
-        ModelView mv = controller.process(paramMap);
+        Map<String, Object> model = new HashMap<>();
 
-        String viewName = mv.getViewName(); //논리이름 new-form
+        String viewName = controller.process(paramMap, model);
+
         MyView view = viewResolver(viewName);
-
-        view.render(mv.getModel(), request, response);
+        view.render(model, request, response);
     }
 
     private MyView viewResolver(String viewName) {
